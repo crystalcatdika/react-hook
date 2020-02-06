1e92442b5c6b8030e5c5d08fa25021f9f61b4319
liyiling <1554233876@qq.com>
2020-02-06 10:34:39 +0800

    useState,useEffect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect }from 'react';
 import './App.css';
 
+const a = 3;
+
 function App() {
-  return (
+  // const [ name, setName ] = useState('yiling');
+  const userName = setInputValue('yiling');
+  const engName = setInputValue('daisy');
+
+
+    setDocumentTitle(userName.value);
+
+
+  const width = getWindowWidth();
+
+
+
+
+
+    return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+            <p>{userName.value} {engName.value}</p>
+            <input
+                { ...userName }
+                // value={name}
+                // onChange={ (e) => { setName(e.target.value)}}
+            />
+            <input
+                { ...engName }
+                // value={name}
+                // onChange={ (e) => { setName(e.target.value)}}
+            />
+            <p>
+                { width }
+
+                {
+                    width > 800 ? 'large' : ''
+                }
+                {
+                    width < 400 ? 'small' : ''
+                }
+            </p>
+        </div>
       </header>
     </div>
   );
 }
+
+function setInputValue(initialValue) {
+  const [ value, setValue ] = useState(initialValue);
+  return {
+    value,
+    onChange: (e) => {setValue(e.target.value)},
+  }
+}
+
+// 注意此处 useEffect 第二个参数 [title] 表示只有title变化时起效，性能优化作用
+// 如果不设置，其他useEffect 导致dom重新渲染，这个useEffect 会起效，打印documentEffect
+
+function setDocumentTitle(title) {
+    useEffect(() => {
+        console.log('documentEffect');
+        document.title = title;
+    },[title]);
+}
+
+function getWindowWidth() {
+    const [ width, setWidth] = useState(window.innerWidth);
+
+    useEffect( () => {
+        console.log('resizeEffect');
+        window.addEventListener('resize', () => {
+            setWidth(window.innerWidth)
+        });
+        return () => {
+            window.removeEventListener('resize', () => {
+                setWidth(window.innerWidth)
+            });
+        }
+    }, [window.innerWidth]);
+    return width;
+}
+
+
+
+
+
 
 export default App;
